e5b3dc760bf9e56f2f90e7e5308f4ccf4c88d788
RaiPeter <rai.peter2@gmail.com>
2020-04-29 03:14:57 +0530

    form setup styles

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,13 +1,28 @@
-FilePond.registerPlugin(
+const rootStyles = window.getComputedStyle(document.documentElement)
+
+if(rootStyles.getPropertyValue('--book-cover-width-large') != null &&
+rootStyles.getPropertyValue('--book-cover-width-large') != ''){
+  ready()
+}else{
+  document.getElementById('main-css').addEventListener('load',ready)
+}
+
+function ready(){
+  const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+  const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+
+
+  FilePond.registerPlugin(
     FilePondPluginImagePreview,
     FilePondPluginImageResize,
     FilePondPluginFileEncode,
   )
   
   FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
+    stylePanelAspectRatio: coverWidth / coverAspectRatio,
     imageResizeTargetWidth: 100,
     imageResizeTargetHeight: 150
   })
   
   FilePond.parse(document.body);
+}
